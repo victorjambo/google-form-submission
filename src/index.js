43751cf945418e90eeecac05105d80e33d1bbc84
victorjambo <victorjambo@live.com>
2020-08-21 10:52:26 +0300

    chore: replace div with nav

--- src/index.js
+++ src/index.js
@@ -3,21 +3,18 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
-import styles from './styles.lazy.css';
+import styles from './styles.lazy.scss';
 
 const createFindNode = () => {
-  styles.use();
-
-  const div = document.createElement('div');
-
-  div.className = styles.locals['container'];
-
-  div.setAttribute('id', 'submissio');
-  document.body.appendChild(div);
+  const nav = document.createElement('nav');
+  nav.className = 'submissio-sidebar';
+  nav.setAttribute('id', 'submissio');
+  document.body.appendChild(nav);
   return document.getElementById('submissio');
 }
 
 document.addEventListener('DOMContentLoaded', function(event) {
+  styles.use();
   const wrapper = createFindNode();
   wrapper ? ReactDOM.render(<App />, wrapper) : false;
 });
