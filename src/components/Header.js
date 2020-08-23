0b1da71c237ff6bc59f653fdf585e2da8151a661
victorjambo <victorjambo@live.com>
2020-08-23 03:01:34 +0300

    chore: header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import * as Icon from 'react-feather';
 
 const Header = () => (
-  <div className="submissio-view-header">
-    Header
+  <div className="submissio-view-header submissio-flex-row">
+    <div className="submissio-header submissio-flex-col">
+      <div className="submissio-header-title">Submissio</div>
+      <div className="submissio-header-username submissio-flex-row">
+        <Icon.User size={10} />
+        <div>
+          Victor Mutai
+        </div>
+      </div>
+    </div>
+    <div className="submissio-header-icons">
+      <Icon.Settings size={15} />
+      <Icon.Sun size={15} />
+      <Icon.Moon size={15} />
+      <Icon.Eye size={15} />
+      <Icon.EyeOff size={15} />
+    </div>
   </div>
 );
 
