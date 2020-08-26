30d12d456855fe352e0a944fd158e5d08b52d291
victorjambo <victorjambo@live.com>
2020-08-27 02:10:44 +0300

    chore: DefaultValues logic

--- src/components/Settings/DefaultValues.js
+++ src/components/Settings/DefaultValues.js
@@ -1,12 +1,82 @@
-import React from 'react';
+/* eslint-disable max-len */
+import React, { useContext, useState } from 'react';
+import * as Icons from 'react-feather';
+import PropTypes from 'prop-types';
 
-const DefaultValues = () => {
-  console.log();
+import { SubmitContext } from '../../context/Submit';
+import { FIELD_TYPES, FIELDS } from '../../utils/formFields';
+import Dropdown from '../elements/Dropdown';
+import BtnGroup from '../elements/BtnGroup';
+
+const Switcher = ({ name, setValue }) => {
+  const [fieldDefaults] = useContext(SubmitContext);
+
+  switch (FIELDS[name].type) {
+    case FIELD_TYPES.DROPDOWN:
+      return <Dropdown defaultItem={fieldDefaults[name]} menuItems={FIELDS[name].options} setState={value => setValue(value)} />;
+    case FIELD_TYPES.GROUP:
+      return <BtnGroup defaultItem={fieldDefaults[name]} menuItems={FIELDS[name].options} setState={value => setValue(value)} />;
+    default:
+      return <input />;
+  }
+};
+
+const DefaultValues = ({ name }) => {
+  const [value, setValue] = useState('');
+  const [isEditting, setEdit] = useState(false);
+  const [, setFieldDefaults, customDisplayname] = useContext(SubmitContext);
+
+  const onSave = ({ name, value }) => {
+    setFieldDefaults({ type: name, value });
+    setEdit(false);
+  };
+
+  if (isEditting) {
+    return (
+      <>
+        <Switcher name={name} setValue={setValue} />
+        <Icons.Save color="#1BC5BD" onClick={() => onSave({ name, value })} />
+        <Icons.X color="#F64E60" onClick={() => setEdit(false)} />
+      </>
+    );
+  }
   return (
-    <div className="container">
-      DefaultValues
-    </div>
+    <>
+      {customDisplayname[name]}
+      <Icons.Edit2 color="#1BC5BD" onClick={() => setEdit(true)} />
+    </>
   );
 };
 
-export default DefaultValues;
+const DefaultValuesContainer = () => {
+  const [fieldDefaults] = useContext(SubmitContext);
+  const newFieldDefaults = { ...fieldDefaults };
+
+  delete newFieldDefaults.url;
+  delete newFieldDefaults.comment;
+  delete newFieldDefaults.icnumber;
+
+  return (
+    <>
+      <h3>Change Default Values</h3>
+      <ul className="list-group">
+        {Object.keys(newFieldDefaults).map(name => (
+          <li key={name} className="list-group-item d-flex justify-content-between align-items-center">
+            <DefaultValues name={name} />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default DefaultValuesContainer;
+
+DefaultValues.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+Switcher.propTypes = {
+  name: PropTypes.string.isRequired,
+  setValue: PropTypes.func.isRequired,
+};
--- src/components/Settings/index.js
+++ src/components/Settings/index.js
@@ -2,19 +2,19 @@ import React, { useState } from 'react';
 
 import DefaultView from '../DefaultView';
 import DisplayNameContainer from './DisplayName';
-import DefaultValues from './DefaultValues';
-import ViewSwitcher, { VIEW_NAMES } from './ViewSwitcher';
+import DefaultValuesContainer from './DefaultValues';
+import SettingsNavigator, { VIEW_NAMES } from './SettingsNavigator';
 
 const Settings = () => {
   const [view, setView] = useState(VIEW_NAMES.HOME);
 
   switch (view) {
     case VIEW_NAMES.HOME:
-      return <ViewSwitcher setView={setView} />;
+      return <SettingsNavigator setView={setView} />;
     case VIEW_NAMES.DISPLAY_NAME:
       return <DisplayNameContainer />;
     case VIEW_NAMES.DEFAULT_VALUES:
-      return <DefaultValues />;
+      return <DefaultValuesContainer />;
     default:
       return <DefaultView />;
   }
